84485ee8d76e78dd440690ac6395ed2c59e8ac64
Letstry2code <104720723+Letstry2code@users.noreply.github.com>
2022-06-29 12:18:21 +0000

    Save

--- Multimedia/reading_img.py
+++ Multimedia/reading_img.py
@@ -1,6 +1,9 @@
-import cv2
+##import cv2
+from PIL import Image
 
-im=cv2.imread('/workspaces/Python-Basic/Multimedia/im 1.png')
-print(im.shape)
-cv2.imshow('Image Window',im)
-cv2.waitKey(0)
+##im=cv2.imread('/workspaces/Python-Basic/Multimedia/im 1.png')
+###print(im.shape)
+##cv2.imshow('Image Window',im)
+##cv2.waitKey(0)
+im33 = Image.open(r'/workspaces/Python-Basic/Capture Error.PNG')
+im33=show()
---
+++ Multimedia/reading_video.py
@@ -0,0 +1,2 @@
+import cv2
+video=cv2.VideoCapture('/workspaces/Python-Basic/Multimedia/WIN_20220629_17_39_04_Pro.mp4')
